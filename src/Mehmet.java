30c417e4127e5fa5421036cd72686dea0614bf7f
Mehmet <meh.korucu@gmail.com>
2022-12-15 22:03:04 +0300

    mythirdcommit

--- src/Mehmet.java
+++ src/Mehmet.java
@@ -2,5 +2,9 @@ public class Mehmet {
     public static void main(String[] args) {
         System.out.println("My first commit");
         System.out.println("My second commit");
+        System.out.println("My third commit");
+
+
+
     }
 }
